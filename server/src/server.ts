7db1a3c812c26076e6ae1c6189b5d8fd80f5ed01
gabrielmaqes <gabrielmaqes@gmail.com>
2020-08-04 22:06:41 -0300

    Database created

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello Node!")
+import express from 'express';
+
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    
+    return response.json({message: "Hello World"});
+});
+
+app.listen(3333);
